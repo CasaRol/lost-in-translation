d603b5a92f1588a70b70b90095396486737a22f4
CasaRol <casarol@PC1681.nordic.local>
2021-03-04 20:46:56 +0100

    Feature: Convertion from text to sign now works wat all letters in the alphabet (no special characters or numbers). Added css to Translation.js

--- src/components/Translation.js
+++ src/components/Translation.js
@@ -1,4 +1,6 @@
-import { useEffect } from 'react'
+import { useState } from 'react'
+
+import '../assets/styles/translation.css'
 
 import a from '../assets/individial_signs/a.png'
 import b from '../assets/individial_signs/b.png'
@@ -58,45 +60,38 @@ function Translation() {
         "z": z
     }
 
-    let imgArray = [
-        images["d"], images["u"], images["b"]
-    ]
-
-    useEffect(() => {
-        console.log("rendering")
-    }, [])
+    const [imageList, setImageList] = useState();
 
     function handleTranslation() {
-        imgArray = []
+
+        let imgArray = []
         const translation = document.getElementById("transInput").value.toLowerCase()
 
         let arr = Array.from(translation)
-
-        //let imgArray = []
 
         arr.forEach(element => {
             if (element === " ") {
                 imgArray.push(" ")
             } else {
-                imgArray.push(element)
+                imgArray.push(images[element])
             }
         });
 
-        console.log(imgArray)
+        setImageList(imgArray)
     }
 
     return (
-        < div >
+        <div>
             <div>
                 <input id="transInput" placeholder="Input your translation" required />
-                <input type="submit" value="Translate" onClick={handleTranslation} />
+                <input id="translate-btn" type="submit" value="Translate" onClick={handleTranslation} />
             </div>
-            <div>
-                {imgArray.map((sign, index) => (
-                    <img key={index} src={sign} alt={index} />
+            <div id="translated">
+                {imageList && imageList.map((sign, index) => (
+                    <img key={index} src={sign} alt="Give up" />
                 ))}
             </div>
-        </div >
+        </div>
     )
 }
 
