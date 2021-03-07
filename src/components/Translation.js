101b892e6d1a01de382682358b31a0bfe5aaab73
Vytautas Zarauskas <v.zarauskas@gmail.com>
2021-03-07 23:41:45 +0100

    refactor: minor cleanup and  changes for readability

--- src/components/Translation.js
+++ src/components/Translation.js
@@ -4,36 +4,34 @@ import '../assets/styles/translation.css'
 
 function Translation() {
 
-    const [imageList, setImageList] = useState([])
-    const [word, setWord] = useState("")
+    const [signs, setSigns] = useState([])
+    const [textToTranslate, setTextToTranslate] = useState("")
 
     function handleTranslation() {
 
-        let imgArray = []
+        let signArray = []
 
-        let arr = Array.from(word.toLowerCase())
+        let charArray = Array.from(textToTranslate.toLowerCase())
 
-        arr.forEach(element => {
-            if (element === " ") {
-                imgArray.push("dash")
-            } else if (/[a-zA-Z]/.test(element)) {
-                imgArray.push(element)
-            } else {
-                //Escape special characters by skipping them
+        charArray.forEach(char => {
+            if (char === " ") {
+                signArray.push("dash")
+            } else if (/[a-zA-Z]/.test(char)) { // Special characters will be skipped
+                signArray.push(char)
             }
         });
 
-        setImageList(imgArray)
+        setSigns(signArray)
     }
 
     return (
         <div>
             <div>
-                <input id="transInput" onChange={e => setWord(e.target.value)} placeholder="Input your translation" required />
+                <input id="transInput" onChange={e => setTextToTranslate(e.target.value)} placeholder="Input your translation" required />
                 <input id="translate-btn" type="submit" value="Translate" onClick={handleTranslation} />
             </div>
             <div id="translated">
-                {imageList && imageList.map((sign, index) => (
+                {signs && signs.map((sign, index) => (
                     <img key={index} src={`individual_signs/${sign}.png`} alt="Give up" />
                 ))}
             </div>
