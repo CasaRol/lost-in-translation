8485dc1ed8dd5db482822988ecb1172daeec2687
CasaRol <casarol@PC1681.nordic.local>
2021-03-08 14:35:26 +0100

    Refactor: Removed doupliacate folder after merge. New merge has been superficially tested for functionality

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import AppHeader from "./components/AppHeader.js"
 import Login from "./components/Login.js"
 import "./App.css"
 import { getStorage } from "./utils/localStorage"
+import Translation from "./components/Translation"
 
 function App() {
-	const [username, setUsername] = useState("")
+  const [username, setUsername] = useState("")
 
-	function handleLoginClicked() {
-		setUsername(getStorage("username"))
-	}
+  function handleLoginClicked() {
+    setUsername(getStorage("username"))
+  }
 
-	useEffect(() => {
-		setUsername(getStorage("username"))
-	}, [])
+  useEffect(() => {
+    setUsername(getStorage("username"))
+  }, [])
 
-	return (
-		<div className="App">
-			<AppHeader username={username} updateUsername={setUsername}/>
-			<Login loginClicked={handleLoginClicked} />
-		</div>
-	)
+  return (
+    <div className="App">
+      <AppHeader username={username} updateUsername={setUsername} />
+      <Login loginClicked={handleLoginClicked} />
+      <Translation></Translation>
+    </div>
+  )
 }
 
 export default App
